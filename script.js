bd2515750adc5687078b5821f4adbba2666e8645
ViniciusBolonheze <appcfaprosol@gmail.com>
2026-07-12 18:28:08 -0300

    Integração do nome pela URL

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Ficha CFA Prosol carregada.");
+const params = new URLSearchParams(window.location.search);
+
+function preencherCampo(id, parametro){
+
+    const elemento = document.getElementById(id);
+
+    if(!elemento) return;
+
+    const valor = params.get(parametro);
+
+    if(valor){
+        elemento.textContent = decodeURIComponent(valor);
+    }
+
+}
+
+preencherCampo("nome","nome");
