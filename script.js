d6523062e42f9d418666db1d13a82ea5703e79c7
ViniciusBolonheze <appcfaprosol@gmail.com>
2026-07-12 22:18:26 -0300

    Atualiza integração dos parâmetros da ficha

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// =======================================
+// CFA PROSOL
+// LEITOR DE PARÂMETROS DA URL
+// =======================================
+
 const params = new URLSearchParams(window.location.search);
+
+//========================================
 
 function preencherCampo(id, parametro){
 
@@ -8,10 +15,51 @@ function preencherCampo(id, parametro){
 
     const valor = params.get(parametro);
 
-    if(valor){
-        elemento.textContent = decodeURIComponent(valor);
-    }
+    if(valor === null) return;
+
+    elemento.textContent = valor;
 
 }
 
+//========================================
+
 preencherCampo("nome","nome");
+
+preencherCampo("posicao","posicao");
+
+preencherCampo("dataNascimento","nascimento");
+
+preencherCampo("dataAv","dataAv");
+
+preencherCampo("altura","altura");
+
+preencherCampo("peso","peso");
+
+preencherCampo("imc","imc");
+
+preencherCampo("gordura","gordura");
+
+preencherCampo("sub","sub");
+
+preencherCampo("tri","tri");
+
+preencherCampo("supra","supra");
+
+preencherCampo("abd","abd");
+
+preencherCampo("predita","predita");
+
+//========================================
+// FOTO
+//========================================
+
+const foto = params.get("foto");
+
+if (foto) {
+
+    const img = document.getElementById("fotoAtleta");
+
+    // Força o navegador a reconstruir a URL
+    img.src = new URL(foto).href;
+
+}
